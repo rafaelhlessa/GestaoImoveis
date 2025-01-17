be82bfe3918f8c73fda03ddf2aafae6ce87919d6
Rafael Lessa <rafaelhlessa@gmail.com>
2025-01-17 06:43:42 -0300

    Trabalhando na DashBoard de Prestadores de serviço

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { ref } from 'vue';
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 import Dropdown from '@/Components/Dropdown.vue';
 import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
@@ -23,9 +22,7 @@ const showingNavigationDropdown = ref(false);
                             <!-- Logo -->
                             <div class="flex shrink-0 items-center">
                                 <Link :href="route('dashboard')">
-                                    <ApplicationLogo
-                                        class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200"
-                                    />
+                                    <img src="/storage/logo-sem fundo.png" class="h-auto w-20" alt="Logo" />
                                 </Link>
                             </div>
 
--- resources/js/Pages/Teste/Teste.vue
+++ resources/js/Pages/Teste/Teste.vue
@@ -1,8 +1,9 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import { Head, router } from '@inertiajs/vue3';
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 import { EllipsisHorizontalIcon } from '@heroicons/vue/20/solid'
+import { watch, defineProps} from 'vue';
 
 const statuses = {
   Paid: 'text-green-700 bg-green-50 ring-green-600/20',
@@ -14,26 +15,26 @@ const props = defineProps({
     serviceProviders: Array,
 });
 
-const clients = [
-  {
-    id: 1,
-    name: 'Proprietário',
-    imageUrl: 'https://tailwindui.com/plus/img/logos/48x48/tuple.svg',
-    lastInvoice: { total: '9', last_year: '12', status: '3' },
-  },
-  {
-    id: 2,
-    name: 'Arrendatário',
-    imageUrl: 'https://tailwindui.com/plus/img/logos/48x48/savvycal.svg',
-    lastInvoice: { total: '4', last_year: '0', status: '2' },
-  },
-  {
-    id: 3,
-    name: 'Parceria Agrícola',
-    imageUrl: 'https://tailwindui.com/plus/img/logos/48x48/reform.svg',
-    lastInvoice: { total: '4', last_year: '6', status: '1' },
-  },
-]
+const property = (id) => {
+    router.get(route('property.index', id));
+}
+
+// Função para aplicar a máscara de Telefone
+const applyPhoneMask = (value) => {
+    const numericValue = value.replace(/\D/g, '');
+
+    if (numericValue.length <= 10) {
+        // TELEFONE: (00) 0000-0000
+        return numericValue
+            .replace(/(\d{2})(\d)/, '($1) $2')
+            .replace(/(\d{4})(\d)/, '$1-$2')
+    } else {
+        // CELULAR: (00) 0 0000-0000
+        return numericValue
+            .replace(/(\d{2})(\d)/, '($1) $2')
+            .replace(/(\d{1})(\d{4})(\d)/, '$1 $2-$3');
+    }
+};
 </script>
 
 <template>
@@ -52,24 +53,62 @@ const clients = [
                 <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
                     <div class="p-6 text-gray-900 dark:text-gray-100">
                         <div class="bg-white rounded-lg shadow">
-                            <h1 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-800 p-4">Clientes</h1>
                             <div class="mx-auto max-w-2xl px-4 py-10 sm:px-6 sm:py-10 lg:max-w-7xl lg:px-8">
-                                <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
-                                    <div v-for="client in props.serviceProviders" :key="client">
-                                        <div class="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400">
-                                            <div class="shrink-0">
-                                                <img class="size-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-                                                </div>
-                                                <div class="min-w-0 flex-1">
-                                                <a href="#" class="focus:outline-none">
-                                                    <span class="absolute inset-0" aria-hidden="true"></span>
-                                                    <p class="text-sm font-medium text-gray-900">{{client.owner.name}}</p>
-                                                    <p class="truncate text-sm text-gray-500">{{ client.owner.phone }}</p>
+                                <div class="bg-white ">
+                                    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+                                        <div class="mx-auto max-w-2xl lg:mx-0">
+                                            <h2 class="text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">Seus Clientes</h2>
+                                            <p class="mt-6 mb-6 text-lg/8 text-gray-600">Lista dos clientes, proprietários que concederam acesso as informações das suas propriedades.</p>
+                                        </div>
+                                            <ul role="list" v-if="props.serviceProviders" v-for="client in props.serviceProviders" :key="client" class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+                                                <a href="#" class="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow">
+                                                    <li @click="property(client.owner.id)" class="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow">
+                                                        <div class="flex w-full items-center justify-between space-x-6 p-6">
+                                                        <div class="flex-1 truncate">
+                                                            <div class="flex items-center space-x-3">
+                                                            <h3 class="truncate text-sm font-medium text-gray-900">{{ client.owner.name }}</h3>
+                                                            <span v-if="client.owner.profile_id === 1" class="inline-flex shrink-0 items-center rounded-full bg-green-50 px-1.5 py-0.5 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">Proprietário</span>
+                                                            </div>
+                                                            <p class="mt-1 truncate text-sm text-gray-500">{{client.owner.city}}</p>
+                                                        </div>
+                                                        <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-10 shrink-0 rounded-full bg-gray-600">
+                                                            <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                                        </svg> -->
+
+                                                        <img class="size-10 shrink-0 rounded-full bg-gray-300 ring-2 ring-gray-300" src="/storage/user.jpg" alt="">
+                                                        </div>
+                                                        <div>
+                                                        <div class="-mt-px flex divide-x divide-gray-200">
+                                                            <div class="flex w-0 flex-1">
+                                                            <a href="mailto:janecooper@example.com" class="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900">
+                                                                <svg class="size-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+                                                                <path d="M3 4a2 2 0 0 0-2 2v1.161l8.441 4.221a1.25 1.25 0 0 0 1.118 0L19 7.162V6a2 2 0 0 0-2-2H3Z" />
+                                                                <path d="m19 8.839-7.77 3.885a2.75 2.75 0 0 1-2.46 0L1 8.839V14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V8.839Z" />
+                                                                </svg>
+                                                                {{client.owner.email}}
+                                                            </a>
+                                                            </div>
+                                                            <div class="-ml-px flex w-0 flex-1">
+                                                            <a href="tel:+1-202-555-0170" class="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900">
+                                                                <svg class="size-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+                                                                <path fill-rule="evenodd" d="M2 3.5A1.5 1.5 0 0 1 3.5 2h1.148a1.5 1.5 0 0 1 1.465 1.175l.716 3.223a1.5 1.5 0 0 1-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 0 0 6.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 0 1 1.767-1.052l3.223.716A1.5 1.5 0 0 1 18 15.352V16.5a1.5 1.5 0 0 1-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 0 1 2.43 8.326 13.019 13.019 0 0 1 2 5V3.5Z" clip-rule="evenodd" />
+                                                                </svg>
+                                                                {{applyPhoneMask(client.owner.phone)}}
+                                                            </a>
+                                                            </div>
+                                                        </div>
+                                                        </div>
+                                                    </li>
                                                 </a>
-                                            </div>
+                                            </ul>
+                                            <div v-else class="flex items-center justify-center h-96">
+                                                <div class="text-center">
+                                                    <h2 class="text-2xl font-semibold text-gray-900">Nenhum cliente encontrado</h2>
+                                                    <p class="mt-2 text-sm text-gray-600">Não há clientes cadastrados.</p>
+                                                </div>
+                                            </div>    
                                         </div>
                                     </div>
-                                </div>
                             </div>
                         </div>
                     </div>
